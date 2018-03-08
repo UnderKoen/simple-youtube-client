ccb39c12bcc9831a4b775914ef78059ed1afedb4
Under_Koen <koenvanstaveren@hotmail.com>
2018-03-08 20:56:53 +0100

    duplicate code

--- src/main/java/nl/underkoen/simple_youtube_chat/entities/SuperChatDetails.java
+++ src/main/java/nl/underkoen/simple_youtube_chat/entities/SuperChatDetails.java
@@ -5,15 +5,7 @@ import com.google.gson.annotations.SerializedName;
 /**
  * Created by Under_Koen on 08/03/2018.
  */
-public class SuperChatDetails {
-    @SerializedName("amountMicros")
-    public long amountMicros;
-    @SerializedName("currency")
-    public String currency;
-    @SerializedName("amountDisplayString")
-    public String amountDisplayString;
-    @SerializedName("userComment")
-    public String userComment;
+public class SuperChatDetails extends FanFundingEventDetails {
     @SerializedName("tier")
     public int tier;
 }
